8c0161b181e8447f1606f51dc54b28f7881a2492
Lukas Bachschwell <lukas@lbsfilm.at>
2020-09-27 13:08:47 +0200

    Fix prepare fs example
    Signed-off-by: Lukas Bachschwell <lukas@lbsfilm.at>

--- examples/prepareFilesystem/prepareFilesystem.ino
+++ examples/prepareFilesystem/prepareFilesystem.ino
@@ -1,11 +1,11 @@
 #include <ESPUI.h>
 
-ESPUIClass ESPUI( Verbosity::VerboseJSON );
-
-void setup(void) {
+void setup(void)
+{
   Serial.begin(115200);
   ESPUI.prepareFileSystem();
 }
 
-void loop() {
+void loop()
+{
 }
