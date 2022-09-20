90ba90a717f3c779a81512a1c29ee12465ab620f
Nick Reynolds <nick@arcanium.london>
2022-09-20 15:29:34 +0100

    Make captive portal redirect on by default.

--- src/ESPUI.h
+++ src/ESPUI.h
@@ -236,7 +236,7 @@ public:
     unsigned int jsonUpdateDocumentSize;
     unsigned int jsonInitialDocumentSize;
     bool sliderContinuous;
-	bool captivePortal = false;
+	bool captivePortal = true;
 
     void setVerbosity(Verbosity verbosity);
     void begin(const char* _title, const char* username = nullptr, const char* password = nullptr,
